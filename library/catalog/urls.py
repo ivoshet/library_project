6c530f612d805b118a95140a6a8b49e2255cc14b
ivoshet <ivoshet.world@gmail.com>
2020-02-02 12:12:58 +0700

    added settings and urls

---
+++ library/catalog/urls.py
@@ -0,0 +1,7 @@
+# catalog/url.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+]
--- library/library/urls.py
+++ library/library/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# link to catalog/urls.py
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+# link for empty request
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True))
+]
+
+# settings for static file, analog settings in settings.py
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
